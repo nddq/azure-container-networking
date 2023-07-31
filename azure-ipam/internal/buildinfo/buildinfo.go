aee4f2eac9a38c1eae2afa4f53e9cc30ae17fbcf
Quang Nguyen <nddangquang@gmail.com>
2023-07-31 22:09:59 +0000

    revert file and var naming, add correct path to makefile

--- azure-ipam/internal/buildinfo/buildinfo.go
+++ azure-ipam/internal/buildinfo/buildinfo.go
@@ -1,6 +1,6 @@
-package buildversion
+package buildinfo
 
 // this will be populate by the Go compiler via
 // the -ldflags, which insert dynamic information
 // into the binary at build time
-var BuildVersion string
+var Version string
