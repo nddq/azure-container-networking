68e65e70bcc3ad914865acaebfa20db01060c2bd
Quang Nguyen <nddangquang@gmail.com>
2023-08-29 16:10:47 +0000

    rebase

--- azure-ipam/internal/buildversion/buildversion.go
+++ azure-ipam/internal/buildversion/buildversion.go
@@ -1,6 +1,6 @@
-package buildinfo
+package buildversion
 
 // this will be populate by the Go compiler via
 // the -ldflags, which insert dynamic information
 // into the binary at build time
-var Version string
+var BuildVersion string
